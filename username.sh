bcd1f6140bd86e8da80d17f22b311024afb35cae
Tyler L <69596889+tyeleur@users.noreply.github.com>
2020-11-16 10:29:56 -0800

    Update username.sh

--- username.sh
+++ username.sh
@@ -7,7 +7,7 @@ echo " It must start with a lower case character"
 echo " It must contain at least 3 but not more than 12 characters"
 
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[abcdefghijklmnopqrstuvwxyz]"+"[_][0-9]" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[abcdefghijklmnopqrstuvwxyz][_][0-9]" > /dev/null 2>&1
 do
 	echo "You must enter a valid username -"
 	echo "Follow the paramaters above! "
