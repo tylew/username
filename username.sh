79a8fc2dcdce9f6b018a88d5489deed8459e43c6
tylerlewissb@gmail.com <lewis@Tylers-MacBook-Pro.local>
2020-11-15 20:50:53 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 # username.sh
-# author
+# Tyler Lewis
 echo "Enter a username: "
-echo "User lower case characters, digits and underscore"
+echo " Use lower case characters, digits and underscore"
 echo " It must start with a lower case character"
 echo " It must contain at least 3 but not more than 12 characters"
 
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[abcdefghijklmnopqrstuvwxyz]"+"[_][0-9]" > /dev/null 2>&1
 do
-	echo "You must enter a valid username - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter a valid username -"
+	echo "Follow the paramaters above! "
 	read USERNAME
 done
 echo "Thank you"
