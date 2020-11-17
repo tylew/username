663c6db5a7e8f8b7aad6c2d59f32eb6ec74b0bcb
tylerlewissb@gmail.com <lewis@Tylers-MacBook-Pro.local>
2020-11-17 09:28:03 -0800

    "adds username.sh"

--- username.sh
+++ username.sh
@@ -7,7 +7,7 @@ echo " It must start with a lower case character"
 echo " It must contain at least 3 but not more than 12 characters"
 
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z][a-z_0-9]{2-11}" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[abcdefghijklmnopqrstuvwxyz][abcdefghijklmnopqrstuvwxyz_01233456789]{2,11}" > /dev/null 2>&1
 do
 	echo "You must enter a valid username -"
 	echo "Follow the paramaters above! "
